c05d96a9c9f913f8158b3ee8ebddcec712ada64d
gastvsp <gastvsp@R-110.cadlab.tu-berlin.de>
2019-04-10 11:52:44 +0200

    fixed naming mistake

--- src/test/java/org/matsim/class2019/CollectionTest.java
+++ src/test/java/org/matsim/class2019/CollectionTest.java
@@ -12,7 +12,7 @@ public class CollectionTest {
 		mylist.add(new Rectangle(10, 20));
 		mylist.add(new Rectangle(10, 200));
 		mylist.add(new Rectangle(10, 2));
-		for(Rectangle rectangle : mylist) {
-			System.out.println("The area is : " + rect.calculateArea()) ;
+		for(Rectangle rect : mylist) {
+			System.out.println("The area is : " + rect.calculate_area()) ;
 		}
 }}
